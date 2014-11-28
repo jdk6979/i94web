56f249f4efb9f40ec2d73fabab952d4fb6b86237
Yuan Kang <dwqs@users.noreply.github.com>
2014-11-28 11:00:18 +0800

    Update common.inc.php

--- includes/common.inc.php
+++ includes/common.inc.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL^E_WARNING^E_NOTICE);
 /*创建转义常量,判断是否需要转义，建议需要存入数据库或者与数据库有交互的数据都进行转义*/
 define('GPC', get_magic_quotes_gpc());
 // 定义网站根目录的常量
-define("HOST", "http://localhost/i94web");
+define("HOST", "网站根目录");
 /*转换硬路径，引入文件速度比require快*/
 define('ROOT_PATH',substr(dirname(__FILE__), 0,-9));
 
@@ -24,10 +24,10 @@ if(PHP_VERSION < '4.1.0')
 require_once ROOT_PATH.'/includes/mysql.func.php';
 
 // 数据库常量定义
-define('DB_USER', 'root');
-define('DB_PWD', '2012082042');
-define('DB_HOST', 'localhost:3307');
-define('DB_NAME', 'i94web');
+define('DB_USER', '用户名');
+define('DB_PWD', '密码');
+define('DB_HOST', '主机');
+define('DB_NAME', '数据库名');
 
 // 数据库初始化
 connectMySQL();
